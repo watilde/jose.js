b96ad4fd34883f6921aaa36717bef7e6d22b8917
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-01-31 19:18:57 +0900

    first commit

---
+++ src/jwt.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+  var crypto = require('crypto');
+  var JWS = require('./JWS');
+  var JWE = require('./JWE');
+  var URLSafeBase64 = require('./URLSafeBase64');
+};
