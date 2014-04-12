3b3c9830f6662b3ec89787e65caa9ef49d602f9b
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-04-13 01:02:37 +0900

    Add scaffold

---
+++ README.md
@@ -0,0 +1,1 @@
+http://datatracker.ietf.org/wg/jose/
--- src/jwt.js
+++
@@ -1,6 +0,0 @@
-module.exports = function () {
-  var crypto = require('crypto');
-  var JWS = require('./JWS');
-  var JWE = require('./JWE');
-  var URLSafeBase64 = require('./URLSafeBase64');
-};
